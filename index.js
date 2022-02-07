abf3e78b917d1e6e6d09d750a3157e17431530d0
muehlbver <vera.muehlberger@brgenns.ac.at>
2022-02-07 15:36:28 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-const express = require ('express')
-const app = express ()
+const express = require('express')
+const app = express()
+
 const server = require ('http').createServer(app)
+
 const port = 3000
 
 const axios = require('axios')
 const pokeapi = 'https://pokeapi.co/api/v2/'
 
-server.listen (port, () => {
-  console.log('Webserver läuft. Port:%d', port)
+server.listen(port, function() {
+
+  console.log('Websaver läuf. Pott: %d', port)
 })
 
-app.get ('/', (req,res) => {
-//console.log(req)
-//res.send('Hällo ägän')
+app.get('/', (req, res) => {
+//console.log(req);
+//res.send('hällo ägän (not)')
 axios
-.get(pokeapi + 'pokemon/ditto')
-.then(response => {
-  res.send(
-    '>img src="' +
-    response.data.sprites.other ['official-artwork'].front_default +
-    '">'
-  )
+  .get(pokeapi + 'pokemon/zekrom')
+  .then(response =>{
+     res.send
+     ('<img src= " ' +
+  response.data.sprites.other['official-artwork'].front_default +
+  '">'
+     );
+}).catch(error => {
+  console.log(error)
+  res.send(error)
 })
-.catch((error) => {
-console.log(error)
-res.send(error)
-  })
-})
+});
